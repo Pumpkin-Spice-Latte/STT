121d50cb3813f01f3e3f605e5266b296e01d40bb
ingramtolish <45722898+ingramtolish@users.noreply.github.com>
2019-11-27 21:08:46 -0500

    Update s2tdb.sql

--- dbScript/s2tdb.sql
+++ dbScript/s2tdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2019 at 05:59 AM
+-- Generation Time: Nov 28, 2019 at 03:07 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.5.19
 
@@ -110,19 +110,6 @@ INSERT INTO `codebook_hdr` (`codebook_id`, `codebook_name`, `account_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `counts`
---
-
-CREATE TABLE IF NOT EXISTS `counts` (
-  `CID` int(11) NOT NULL,
-  `SID` int(11) NOT NULL,
-  `Uword` varchar(255) DEFAULT NULL,
-  `wordCount` int(11) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `session`
 --
 
@@ -131,6 +118,31 @@ CREATE TABLE IF NOT EXISTS `session` (
   `account_id` int(11) DEFAULT NULL,
   `codebook_id` int(11) DEFAULT NULL,
   `session_name` varchar(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `session`
+--
+
+INSERT INTO `session` (`session_id`, `account_id`, `codebook_id`, `session_name`) VALUES
+(3, 14, 15, 'test'),
+(4, 14, 14, 'test2'),
+(5, 14, 15, 'test3'),
+(6, 14, 16, 'test7'),
+(7, 14, 20, 'test56'),
+(8, 14, 20, 'tet2'),
+(9, 14, 20, 'tew');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session_counts`
+--
+
+CREATE TABLE IF NOT EXISTS `session_counts` (
+  `session_id` int(11) NOT NULL,
+  `detail_id` int(11) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -184,7 +196,7 @@ MODIFY `codebook_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
-MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
