73d0a99fac735c356624dcad0eae00196f549b5a
ingramtolish <45722898+ingramtolish@users.noreply.github.com>
2019-11-30 01:22:47 -0500

    Cleanup 'off by 1' error when creating codebook details

--- src/Components/CodebookFactory.java
+++ src/Components/CodebookFactory.java
@@ -47,13 +47,13 @@ public class CodebookFactory {
 				String startWord = "";
 				String endWord = "";
 				if (detail.contains("...")) {					
-					startWord = detail.substring(0, detail.indexOf(" ..."));
-					endWord = detail.substring(detail.indexOf(" ...") + 4);
+					startWord = detail.substring(1, detail.indexOf(" ..."));
+					endWord = detail.substring(detail.indexOf(" ...") + 5);
 				} else {
-					startWord = detail;
+					startWord = detail.trim();
 				}
 				if (startWord.trim().length() > 0)
-					objCodebookDAO.insertIntoCodebookDtl(codebookHeaderID, startWord.trim(), endWord.trim());
+					objCodebookDAO.insertIntoCodebookDtl(codebookHeaderID, startWord, endWord);
 			}	
 			objConnection.commit(); //Final commit
 
